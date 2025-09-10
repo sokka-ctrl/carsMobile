f5ef97b92686298aad677611f6a1a956932da0e4
sokka-ctrl <kotermix123@gmail.com>
2025-09-10 12:19:58 +0600

    app update with Fragments

--- app/src/main/java/com/example/carsmobile/carAdapter.kt
+++ app/src/main/java/com/example/carsmobile/carAdapter.kt
@@ -3,11 +3,10 @@ package com.example.carsmobile
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
 import com.bumptech.glide.Glide
 import com.example.carsmobile.databinding.CarsWidjetsItemBinding
 
-class car_Adapter(val list_car: MutableList<cars_items>) : RecyclerView.Adapter<car_Adapter.CarViewHolder>(){
+class carAdapter(val list_car: MutableList<cars_items>) : RecyclerView.Adapter<carAdapter.CarViewHolder>(){
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -18,7 +17,7 @@ return CarViewHolder(CarsWidjetsItemBinding.inflate(LayoutInflater.from(parent.c
     override fun onBindViewHolder(
         holder: CarViewHolder,
         position: Int
-    ) {
+    ){
     holder.onBind(list_car[position])
         holder.itemView.setOnClickListener {
             view ->
@@ -33,14 +32,12 @@ return CarViewHolder(CarsWidjetsItemBinding.inflate(LayoutInflater.from(parent.c
     class CarViewHolder(private val binding: CarsWidjetsItemBinding): RecyclerView.ViewHolder(binding.root){
 
         fun onBind(carsItems: cars_items){
-            binding.carModel.text =  carsItems.carModel
-            binding.carType.text = carsItems.carType
-            binding.carSpeed.text = carsItems.carSpeed
-            binding.carCompany.text = carsItems.carCompany
-            binding.StarsWidget.text = carsItems.carStars
-            Glide.with(binding.carIMG).load(carsItems.imageCar).into(binding.carIMG)
-
-
+            binding.tvCarModel.text =  carsItems.carModel
+            binding.tvCarType .text = carsItems.carType
+            binding.tvCarSpeed .text = carsItems.carSpeed
+            binding.tvCarCountry .text = carsItems.carCompany
+            binding.tvStarsWidget .text = carsItems.carStars
+            Glide.with(binding.ivCarImage).load(carsItems.imageCar).into(binding.ivCarImage)
         }
     }
 }
